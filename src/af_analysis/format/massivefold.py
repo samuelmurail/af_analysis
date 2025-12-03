658a0be354fccf7c5769046fc7d6495d6ba44655
samuelmurail <samuel.murail@univ-paris-diderot.fr>
2025-12-03 14:47:19 +0100

    Fix the issue with af3 outputs

--- src/af_analysis/format/massivefold.py
+++ src/af_analysis/format/massivefold.py
@@ -37,27 +37,83 @@ def read_dir(directory):
     query = pred_dir.split("/")[-1]
 
     for file in os.listdir(pred_dir):
-        if file.endswith(".pdb"):
-            tokens = file[:-4].split("_")
-            model = int(tokens[4])
-            weight = tokens[5] + "_" + tokens[6]
-            seed = tokens[-1]
+        logger.info(f"Processing file: {file}")
+        if file.endswith(".pdb") or file.endswith(".cif"):
 
-            pkl_score = os.path.join(
-                pred_dir,
-                f"result_model_{model}_{weight}_pred_{seed}.pkl",
-            )
-            if not os.path.exists(pkl_score):
+            confidence_score = None
+
+            if not file.find("af3") != -1:
+                tokens = file[:-4].split("_")
+                model = int(tokens[4])
+                weight = tokens[5] + "_" + tokens[6]
+                seed = int(tokens[-1])
+
                 pkl_score = os.path.join(
                     pred_dir,
-                    f"light_pkl/result_model_{model}_{weight}_pred_{seed}.pkl",
+                    f"result_model_{model}_{weight}_pred_{seed}.pkl",
                 )
+                if not os.path.exists(pkl_score):
+                    pkl_score = os.path.join(
+                        pred_dir,
+                        f"light_pkl/result_model_{model}_{weight}_pred_{seed}.pkl",
+                    )
+            else: # Special case for af3
+                tokens = file[:-4].split("_")
+                model = int(tokens[4]) # In reality it is a the seed
+                weight = "af3"
+                seed = int(tokens[-3])
+                pred_num = int(tokens[-1])
+                rank = int(tokens[1])
+
+                pkl_score = os.path.join(
+                    pred_dir,
+                    f"result_model_{model}_{weight}_pred_{seed}.pkl",
+                )
+                if not os.path.exists(pkl_score):
+                    pkl_score = os.path.join(
+                        pred_dir,
+                        f"light_pkl/result_af3_seed_{model}_sample_{seed}_pred_{pred_num}.pkl",
+                    )
+                    confidence_score = os.path.join(
+                        pred_dir,
+                        f"confidences/ranked_{rank}_af3_seed_{model}_sample_{seed}_pred_{pred_num}.json",
+                    )
+                    with open(confidence_score, "r") as json_file:
+                        json_data = json.load(json_file)
+                        # print(json_data.keys())
+
             if not os.path.exists(pkl_score):
                 logger.warning(f"Score file {pkl_score} does not exist.")
                 continue
 
             np_score = np.load(pkl_score, allow_pickle=True)
 
+            if "plddt" in np_score:
+                plddt = np_score["plddt"].mean()
+            else:
+                plddt = None
+            
+            if "ptm" in np_score:
+                ptm = float(np_score["ptm"])
+            elif confidence_score is not None:
+                ptm = json_data["ptm"]
+            else:
+                ptm = None
+            
+            if "iptm" in np_score:
+                iptm = float(np_score["iptm"])
+            elif confidence_score is not None:
+                iptm = json_data["iptm"]
+            else:
+                iptm = None
+                
+            if "ranking_confidence" in np_score:
+                ranking_confidence = float(np_score["ranking_confidence"])
+            elif confidence_score is not None:
+                ranking_confidence = json_data["ranking_score"]
+            else:
+                ranking_confidence = None
+            
             if "num_recycles" not in np_score:
                 np_score["num_recycles"] = -1
 
@@ -68,10 +124,10 @@ def read_dir(directory):
                 "model": model,
                 "weight": weight,
                 "recycle": int(np_score["num_recycles"]),
-                "pLDDT": np_score["plddt"].mean(),
-                "pTM": float(np_score["ptm"]),
-                "ipTM": float(np_score["iptm"]),
-                "ranking_confidence": float(np_score["ranking_confidence"]),
+                "pLDDT": plddt,
+                "pTM": ptm,
+                "ipTM": iptm,
+                "ranking_confidence": ranking_confidence,
                 "data_file": pkl_score,
             }
 
