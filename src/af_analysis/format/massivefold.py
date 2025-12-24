2189bf563ef53b9d343d84e5477630a415a54e7e
NessimRaou <nessim.raouraoua.etu@univ-lille.fr>
2025-12-24 21:58:55 +0100

    Scores for massivefold format: extract them from json ranking files by default, pkl if need and if present

--- src/af_analysis/format/massivefold.py
+++ src/af_analysis/format/massivefold.py
@@ -35,10 +35,33 @@ def read_dir(directory):
 
     pred_dir = directory
     query = pred_dir.split("/")[-1]
+    ranking_files = [ 
+        os.path.join(pred_dir, i) 
+        for i in os.listdir(pred_dir) 
+        if i.startswith('ranking_') and i.endswith('.json') 
+    ]
 
+    # get metrics preferably from massivefold ranking files instead of pickles
+    global_df = pd.DataFrame()
+    for ranking in ranking_files:
+        json_ranking = json.load(open(ranking, 'r'))
+        ranking_metric = list(json_ranking.keys())[0]
+        models_metric = json_ranking[ranking_metric]
+        metric_df = pd.DataFrame(models_metric.items(), columns=['prediction', ranking_metric])
+        if global_df.empty:
+            global_df = metric_df
+        else:
+            global_df = global_df.merge(metric_df, on="prediction")
+
+    # compute ranking_confidence for AF3 which has a different 'ranking_score'
+    if ({'iptm', 'ptm'}.issubset(set(global_df.columns))) and ('ranking_confidence' not in global_df.columns):
+        global_df["ranking_confidence"] = 0.8*global_df["iptm"] + 0.2*global_df["ptm"]
+
+    pkl_not_found = []
     for file in os.listdir(pred_dir):
         logger.info(f"Processing file: {file}")
         if file.endswith(".pdb") or file.endswith(".cif"):
+            is_pkl = True
             # print(f"Processing file: {file}")
 
             confidence_score = None
@@ -62,65 +85,82 @@ def read_dir(directory):
                         pred_dir,
                         f"light_pkl/result_model_{model}_{weight}_pred_{seed}.pkl",
                     )
-            else:  # Special case for af3
+            else: # Special case for af3
                 tokens = file[:-4].split("_")
-                model = int(tokens[4])  # In reality it is a the seed
+                model = int(tokens[4]) # In reality it is a the seed
                 weight = "af3"
                 seed = int(tokens[-3])
                 pred_num = int(tokens[-1])
                 rank = int(tokens[1])
+                prediction = f"af3_seed_{model}_sample_{seed}_pred_{pred_num}"
 
                 pkl_score = os.path.join(
                     pred_dir,
-                    f"result_model_{model}_{weight}_pred_{seed}.pkl",
+                    f"result_{prediction}.pkl",
                 )
                 if not os.path.exists(pkl_score):
                     pkl_score = os.path.join(
                         pred_dir,
-                        f"light_pkl/result_af3_seed_{model}_sample_{seed}_pred_{pred_num}.pkl",
+                        f"light_pkl/result_{prediction}.pkl",
                     )
                     confidence_score = os.path.join(
                         pred_dir,
-                        f"confidences/ranked_{rank}_af3_seed_{model}_sample_{seed}_pred_{pred_num}.json",
+                        f"confidences/ranked_{rank}_{prediction}.json",
                     )
                     with open(confidence_score, "r") as json_file:
                         json_data = json.load(json_file)
                         # print(json_data.keys())
 
+            ranking_scores = (
+                global_df
+                .set_index("prediction")
+                .to_dict(orient='index')
+            )
             if not os.path.exists(pkl_score):
-                logger.warning(f"Score file {pkl_score} does not exist.")
-                continue
+                pkl_not_found.append(pkl_score)
+                is_pkl = False
+            else:
+                np_score = np.load(pkl_score, allow_pickle=True)
+                print(f"{np_score=}")
 
-            np_score = np.load(pkl_score, allow_pickle=True)
-
-            if "plddt" in np_score:
+            if "mean_plddt" in global_df.columns:
+                plddt = ranking_scores[prediction]["mean_plddt"]
+            elif is_pkl and "plddt" in np_score:
                 plddt = np_score["plddt"].mean()
             else:
                 plddt = None
 
-            if "ptm" in np_score:
+            if "ptm" in global_df.columns:
+                ptm = ranking_scores[prediction]["ptm"]
+            elif is_pkl and "ptm" in np_score:
                 ptm = float(np_score["ptm"])
-            elif confidence_score is not None:
+            elif is_pkl and confidence_score is not None:
                 ptm = json_data["ptm"]
             else:
                 ptm = None
 
-            if "iptm" in np_score:
+            if "iptm" in global_df.columns:
+                iptm = ranking_scores[prediction]["iptm"]
+            elif is_pkl and "iptm" in np_score:
                 iptm = float(np_score["iptm"])
-            elif confidence_score is not None:
+            elif is_pkl and confidence_score is not None:
                 iptm = json_data["iptm"]
             else:
                 iptm = None
 
-            if "ranking_confidence" in np_score:
+            if "ranking_confidence" in global_df.columns:
+                ranking_confidence = ranking_scores[prediction]["ranking_confidence"]
+            elif is_pkl and "ranking_confidence" in np_score:
                 ranking_confidence = float(np_score["ranking_confidence"])
-            elif confidence_score is not None:
+            elif is_pkl and confidence_score is not None:
                 ranking_confidence = json_data["ranking_score"]
             else:
                 ranking_confidence = None
 
-            if "num_recycles" not in np_score:
-                np_score["num_recycles"] = -1
+            if is_pkl and "num_recycles" in np_score:
+                num_recycles = np_score["num_recycles"]
+            else:
+                num_recycles = -1
 
             info_dict = {
                 "pdb": os.path.join(pred_dir, file),
@@ -128,7 +168,7 @@ def read_dir(directory):
                 "seed": seed,
                 "model": model,
                 "weight": weight,
-                "recycle": int(np_score["num_recycles"]),
+                "recycle": int(num_recycles),
                 "pLDDT": plddt,
                 "pTM": ptm,
                 "ipTM": iptm,
@@ -138,20 +178,18 @@ def read_dir(directory):
 
             log_dict_list.append(info_dict)
 
+    if len(pkl_not_found) < 30:
+        logger.warning(f"Non-existing score files (.pkl) ({len(pkl_not_found)}):\n {', '.join(pkl_not_found)}.")
+    elif pkl_not_found:
+        logger.warning(f"Detected {len(pkl_not_found)} non-existing score files (.pkl).")
+
     log_pd = pd.DataFrame(log_dict_list)
 
     # Dirty fix of "ranking_confidence" between 0 and 1.
     # Seems to be ok because ranking confidence is rarely below 1.
-    # log_pd["ranking_confidence"] = log_pd["ranking_confidence"].apply(
-    #     lambda x: x / 100 if x > 1 else x
-    # )
-
-    # In the case of higher ranking_confidence than 1 (Colabfold)
-    # we normalize it to be between 0 and 1
-    if (log_pd["ranking_confidence"].max() > 1) and (
-        "ranking_confidence" in log_pd.columns
-    ):
-        log_pd["ranking_confidence"] = 0.8 * log_pd["ipTM"] + 0.2 * log_pd["pTM"]
+    log_pd["ranking_confidence"] = log_pd["ranking_confidence"].apply(
+        lambda x: x * 100 if x < 1 else x
+    )
 
     # To ensure that tests are consistent across different systems
     # we sort the dataframe by pdb
